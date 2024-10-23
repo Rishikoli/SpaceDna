fb591871ad9c392937ab85793d2044cacb00ead5
vkboi5 <vkirthikjan5@gmail.com>
2024-10-23 21:57:07 +0530

    Added the hyperspace effect into new branch - 'feature1'

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
-import Home from './components/Home'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import Space from './components/Space'; // New WelcomePage component
+
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
-  )
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />  {/* Home page */}
+          <Route path="/space" element={<Space />} />  {/* Welcome page */}
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Space.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Space.css"; // Add styling for the welcome page
+
+const Space = () => {
+  return (
+    <div className="welcome-container">
+      <h1>Welcome to our Solar System</h1>
+    </div>
+  );
+};
+
+export default Space;
